f1656da3a154f4f28a44ed0120d11b79bf78e451
Erik O'Leary <erik.m.oleary@gmail.com>
2013-12-12 12:42:30 -0600

    Added initial implementation of document lookup
    Index
    Modules

---
+++ Documentation.py
@@ -0,0 +1,89 @@
+import sublime, sublime_plugin
+import subprocess, threading
+import json
+
+class ListModulesCommand(sublime_plugin.WindowCommand):
+    """
+    Display list of all modules and their descriptions
+    """
+    path = "Documentation/modules/module_index.json"
+
+    def on_item(self, picked):
+        #TODO - Open the file at that point
+        pass
+
+    def on_done(self, picked):
+        if picked < 0: return
+
+        item     = self.items[picked]
+        module   = item[0]
+        mod_path = "Documentation/modules/" + module + ".json"
+
+        mod_items = []
+        with open(mod_path) as json_data:
+            data = json.load(json_data)
+
+            for obj in data:
+                row = []
+                if obj.get("name"):
+                    row.append(obj["name"])
+                    mod_items.append(row)
+
+                if obj.get("code"):
+                    row.append(obj["code"][0:100])
+
+                if obj.get("description"):
+                    row.append(obj["description"][0:100])
+
+
+        self.window.show_quick_panel(mod_items, self.on_item)
+
+    def run(self):
+        self.items = []
+
+        #Parse items
+        with open(ListModulesCommand.path) as json_data:
+            data = json.load(json_data)
+
+            for obj in data:
+                row = [obj["module"]]
+                if obj.get("description"):
+                    row.append(obj["description"][0:100])
+
+                self.items.append(row)
+
+            self.window.show_quick_panel(self.items, self.on_done)
+
+
+class ListAllCommand(sublime_plugin.WindowCommand):
+    """
+    Display index of all procs/modules/methods/etc in std lib
+    """
+    path = "Documentation/modules/proc_index.json"
+
+    def on_done(self, picked):
+        #TODO - Open file at that point
+        pass
+
+    def run(self):
+        items = []
+
+        with open(ListAllCommand.path) as json_data:
+            data = json.load(json_data)
+
+            for obj in data:
+                row = [
+                    obj["name"],
+                    "import " + obj["module"],
+                    obj["code"][0:100]
+                ]
+
+                if obj.get("desc"):
+                    row.append(obj["desc"][0:100])
+
+                items.append(row)
+
+        self.window.show_quick_panel(
+            sorted(items, key=lambda i:i[0]),
+            self.on_done
+        )

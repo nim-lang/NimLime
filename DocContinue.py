4e17d89b158b2e707009d6d2eea22669664a831e
Clay Sweetser <clay.sweetser@gmail.com>
2014-01-07 11:42:23 -0500

    Merged stuff, made more progress on doc-comment continuations

--- DocContinue.py
+++ DocContinue.py
@@ -1,17 +1,43 @@
 from sublime_plugin import EventListener
 
-# TODO
+# TODO - Features
 # Add double line erasure option
 # Add setting loading
-# Fix commont/doComment distinction
 
-COMMENT_SCOPE = "comment.line.number-sign.nimrod"
-DEBUG = False
+# TODO - Optimizations
+# Erase debug calls
+# Inline recursion checking
+# Reduce rowcol/textpoint conversions
+# Add and use scope selectors for checking?
+#  - To check that previous line is empty
+#  - To check that current line is an empty line after a doc-comment
+
+COMMENT_SCOPE = "comment.line.number-sign.doc-comment.nimrod"
+RECURSION_LEVEL = 0
+DEBUG = True
 
 
 def debug(string):
     if DEBUG:
         print(string)
+
+
+def recursion_limit(func):
+    def aux_recursion_limit(*args, **kwargs):
+        global RECURSION_LEVEL
+        debug("Pre-start recursion level - " + str(RECURSION_LEVEL))
+        RECURSION_LEVEL += 1
+        debug("Post-start recursion level - " + str(RECURSION_LEVEL))
+
+        if RECURSION_LEVEL <= 1:
+            func(*args, **kwargs)
+        else:
+            debug("Recursion limited at level " + str(RECURSION_LEVEL))
+
+        debug("Pre-end recursion level - " + str(RECURSION_LEVEL))
+        RECURSION_LEVEL -= 1
+        debug("Post-end recursion level - " + str(RECURSION_LEVEL))
+    return aux_recursion_limit
 
 
 class CommentListener(EventListener):
@@ -33,43 +59,59 @@ class CommentListener(EventListener):
     def on_deactivated(self, view):
         self.active = False
 
+    @recursion_limit
     def on_modified(self, view):
+
+        # Pre-process stage
         if not self.active:
+            debug("Pre-process failure - Inactive")
             return
+
         rowcol_set = [view.rowcol(s.a) for s in view.sel() if s.empty()]
-
         for row, col in rowcol_set:
-
-            # Stage 2
+            # Stage 1 Checks
+            # Checks if the last history action was a newline insertion.
             command, args, repeats = view.command_history(0, False)
             if (command == "insert" and args["characters"] == '\n'):
-                debug("Stage 2 success - A")
+                debug("Stage 1 success - A")
             elif (command == "paste"):
-                debug("Stage 2 success - B")
+                debug("Stage 1 success - B")
+            else:
+                debug("Stage 1 failure - 1")
+                return
+
+            # Used to determine if the user is undoing the insertion.
+            command, args, repeats = view.command_history(1, False)
+            if command == "continueComment":
+                debug("Stage 1 failure - 2")
+                return
+
+            # Stage 2 Checks
+            # Checks that the current line has only whitespace characters.
+
+            current_line = view.line(view.text_point(row, col))
+            if (col == 0) or (view.substr(current_line).isspace()):
+                debug("Stage 2 success")
             else:
                 debug("Stage 2 failure")
                 return
 
-            command, args, repeats = view.command_history(1, False)
-            if command == "continueComment":
-                return
-
-            # Stage 3
-            current_line = view.line(view.text_point(row, col))
-            if view.substr(current_line).isspace():
+            # Stage 3 Checks
+            # Checks that the previous line had a doc-comment.
+            last_line = view.line(view.text_point(row - 1, 0))
+            debug(view.scope_name(last_line.b))
+            if COMMENT_SCOPE in view.scope_name(last_line.b):
                 debug("Stage 3 success")
             else:
                 debug("Stage 3 failure")
                 return
 
-            # Stage 1
-            last_line = view.line(view.text_point(row - 1, 0))
-            debug(view.scope_name(last_line.b))
-            if COMMENT_SCOPE in view.scope_name(last_line.b):
-                debug("Stage 1 success")
-            else:
-                debug("Stage 1 failure")
-                return
+            # Stage 4 Checks (Optional)
+            # Checks if the previous line has an empty doc-comment.
+            # view.substr(current_line)
+
+            # Text Modification Stage
+            # Simply insert a doc-comment into the current line.
             insertion_edit = view.begin_edit("continueComment")
             view.insert(insertion_edit, view.text_point(row, col), "## ")
             view.end_edit(insertion_edit)

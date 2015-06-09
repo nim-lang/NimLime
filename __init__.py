4d1b8c2bf12e133802329c57299dbc0b2dda0afe
Varriount <Varriount@users.noreply.github.com>
2015-06-08 20:58:06 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
-try:
+import sublime
+
+if int(sublime.version()) >= 3000:
     import NimLime.NimLime as lime
-except ImportError:
+else:
     import NimLime as lime
 
 add_module = lime.add_module

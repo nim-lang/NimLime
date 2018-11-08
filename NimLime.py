b281d191eed5e491900a7e2db444b71c5507a22a
Clay Sweetser <clay.sweetser@gdit.com>
2018-11-08 01:46:06 -0500

    Split comment grammer into separate file, to support nested multiline comments. Fixes #94

--- NimLime.py
+++ NimLime.py
@@ -1,22 +1,2 @@
-# # coding=utf-8
-# """
-# Pseudo-root for the NimLime plugin.
-# This adds the actual NimLime root to the python module path list.
-# """
-# import os
-
-# root_dir = os.path.dirname(os.path.abspath(__file__))
-
-# try:
-#     # Try using the sys_path mechanism provided by package control
-#     from package_control import sys_path
-
-#     sys_path.add(root_dir)
-# except ImportError:
-#     import sys
-
-#     sys_path = sys.path
-#     print("NimLime: Warning, couldn't import package_control.sys_path")
-#     sys.path.append(root_dir)
-
+# coding=utf-8
 from NimLime.core.commands import *
